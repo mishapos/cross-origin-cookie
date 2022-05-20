c319ef832d0e4e2cbe27a2206b71e2591cbda73d
Michail Shaposhnikov <mishapos@microsoft.com>
2022-05-20 08:57:53 -0700

    Added api and front end buttons

---
+++ api/demo.cs
@@ -0,0 +1,29 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Company.Function
+{
+    public static class demo
+    {
+        [FunctionName("demo")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            var response = new HttpResponseMessage();
+            var guid = Guid.NewGuid().ToString();
+            var cookieValue = $"{guid};HttpOnly;Secure;Path=/;SameSite=None";
+            response.Headers.Add("SetCookie", $"HeaderCookie={cookieValue}");
+            return response;
+        }
+    }
+}
